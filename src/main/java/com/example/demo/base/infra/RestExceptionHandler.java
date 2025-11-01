215bc9edfe97df2e5c5573b97dd771c8e3a2efcf
Pedro Henrique Rodrigues de Assis <pedro_henriquedeassis@hotmail.com>
2025-11-01 10:18:14 -0300

    Adição de handler personalizado para validações das DTOs.

--- src/main/java/com/example/demo/base/infra/RestExceptionHandler.java
+++ src/main/java/com/example/demo/base/infra/RestExceptionHandler.java
@@ -4,8 +4,14 @@ import com.example.demo.base.exception.NegocioException;
 import com.example.demo.base.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
@@ -26,5 +32,23 @@ public class RestExceptionHandler {
         ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
 
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        // Itera sobre todos os erros de campo encontrados na exceção
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            // Obtém o nome do campo que falhou na validação
+            String fieldName = ((FieldError) error).getField();
+            // Obtém a mensagem de erro que você definiu na annotation da DTO
+            String errorMessage = error.getDefaultMessage();
+            // Adiciona ao mapa
+            errors.put(fieldName, errorMessage);
+        });
+        
+        return errors;
     }
 }
